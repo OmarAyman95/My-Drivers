a542417c499c4641e5b8d25d71d30f0ecc48c71c
OmarAyman <ommoor642@gmail.com>
2022-12-02 21:42:25 +0200

    EXTI.h for ATMEGA32 MCU

--- MCLAs/ATMEGA32/EXTI_MCAL/EXTI.h
+++ MCLAs/ATMEGA32/EXTI_MCAL/EXTI.h
@@ -1,35 +1,28 @@
 /* 
  * File:   EXTI.h
- * Author: OMAR
+ * Author: OMAR AYMAN
  *
  * Created on November 28, 2022, 3:56 AM
  */
 
 #ifndef EXTI_H
-#define	EXTI_H
+#define  EXTI_H
 
 #include "ATMGA32MCU.h"
 #include "GPIO_MEGA32.h"
 #include <avr/interrupt.h>
 
-//Enable and disable interrupt
 #define GLOBAL_INTERRUPT_ENABLE()      SREG_REG->I =1
 #define GLOBAL_INTERRUPT_DESABLE()     SREG_REG->I =0
 
-
-//interrupt_number
 #define INT_0                           (0U)
 #define INT_1                           (2U)
 #define INT_2                           (3U)
 
-
-//interrupt_modes
 #define RISING_EDGE_MODE                    (0b11U)
 #define FALLING_EDGE_MODE                   (0b10U)
 #define BOTH_EDGES_MODE                     (0b01U)
 #define LOW_LEVEL_MODE                      (0b00U)
-
-
 
 
 typedef struct
@@ -37,7 +30,6 @@ typedef struct
     uint8_t interrupt_no   : 2;
     uint8_t interrupt_mode : 2;
 }Ext_interrupt_t;
-
 
 
 /**================================================================
@@ -54,18 +46,16 @@ typedef struct
  
 **/
 void ExtInterrupt_init(Ext_interrupt_t *ext_object);
-
-
-#ifdef	__cplusplus
+#ifdef   __cplusplus
 extern "C" {
 #endif
 
 
 
 
-#ifdef	__cplusplus
+#ifdef   __cplusplus
 }
 #endif
 
-#endif	/* EXTI_H */
+#endif   /* EXTI_H */
 
